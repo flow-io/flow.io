ec85ef1db192080a1d7f3e9d09b72d22f6765992
kgryte <kgryte@gmail.com>
2014-06-19 11:39:33 -0700

    [UPDATE] add JSON parse and stringify transform stream tests.

---
+++ test/test.streams.json.stringify.js
@@ -0,0 +1,27 @@
+
+// MODULES //
+
+var // Expectation library:
+	chai = require( 'chai' ),
+
+	// Module to be tested:
+	stream = require( './../lib/streams/json/stringify.js' );
+
+
+// VARIABLES //
+
+var expect = chai.expect,
+	assert = chai.assert;
+
+
+// TESTS //
+
+describe( 'streams/json/parse', function tests() {
+
+	it( 'should export a factory function', function test() {
+		expect( stream ).to.be.a( 'function' );
+	});
+
+	// For additional tests, see https://github.com/dominictarr/JSONStream/tree/master/test
+
+});
