4824e910c545c52e403d6e1064d45c4a4facaaaf
kgryte <kgryte@gmail.com>
2014-06-23 14:34:06 -0700

    [UPDATE] add histc out-of-bounds test.

--- test/test.stats.histc.js
+++ test/test.stats.histc.js
@@ -99,4 +99,47 @@ describe( 'stats/histc', function tests() {
 		} // end FUNCTION onRead()
 	});
 
+	it( 'should, when computing the histogram counts of piped data, assign out-of-bounds data to appropriate bins', function test( done ) {
+		var data, expected, rStream,
+			edges = [ -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5 ];
+
+		// Simulate some data...
+		data = [ -1, -100, Number.NEGATIVE_INFINITY, 6, 100, Number.POSITIVE_INFINITY ];
+
+		// Expected counts (histc also returns negative and positive infinity bins which include data which does not fall into any bin defined by the edge vector):
+		expected = [
+			[ Number.NEGATIVE_INFINITY, 3, -0.5 ], // -infinity
+			[ -0.5, 0, 0.5 ],
+			[ 0.5, 0, 1.5 ],
+			[ 1.5, 0, 2.5 ],
+			[ 2.5, 0, 3.5 ],
+			[ 3.5, 0, 4.5 ],
+			[ 4.5, 0, 5.5 ],
+			[ 5.5, 3, Number.POSITIVE_INFINITY ] // +infinity
+		];
+
+		// Create a new histc stream:
+		rStream = hStream()
+			.edges( edges )
+			.stream();
+
+		// Mock reading from the stream:
+		utils.readStream( rStream, onRead );
+
+		// Mock piping a data to the stream:
+		utils.writeStream( data, rStream );
+
+		return;
+
+		/**
+		* FUNCTION: onRead( error, actual )
+		*	Read event handler. Checks for errors and compares streamed data to expected data.
+		*/
+		function onRead( error, actual ) {
+			expect( error ).to.not.exist;
+			assert.deepEqual( actual[ 0 ], expected );
+			done();
+		} // end FUNCTION onRead()
+	});
+
 });
